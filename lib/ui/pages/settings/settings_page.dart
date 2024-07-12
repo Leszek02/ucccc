2627c18a72b97a52c2d4f85460400fc196608245
MercilesSong <rybarczyk.mar@gmail.com>
2024-07-12 15:54:21 +0200

    added buttons to settings page

--- lib/ui/pages/settings/settings_page.dart
+++ lib/ui/pages/settings/settings_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ucccc/ui/widgets/circle_button.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -6,14 +8,32 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.75),
-          title: const Text('Character Editor'),
+          backgroundColor:
+              Theme.of(context).colorScheme.primary.withOpacity(0.75),
+          title: const Text('Settings'),
         ),
-        body: const Center(
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('This is the settings page. Duh'),
+              Padding(
+                padding: const EdgeInsets.all(50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CircleButton(
+                      size: 65,
+                      icon: Icons.lock_open,
+                      onPressed: () {},
+                    ),
+                    CircleButton(
+                      size: 65,
+                      icon: Icons.login_outlined,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
