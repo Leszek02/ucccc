523ae8be7d9bc04372a6e40cb7e6436010691b87
MercilesSong <rybarczyk.mar@gmail.com>
2024-08-06 16:43:33 +0200

    Add the name field to the Template class

--- lib/data/template.dart
+++ lib/data/template.dart
@@ -1,16 +1,22 @@
 class Template {
+  String _name;
   List<(String, List<(String, String?)>)> objects;
-  final List<(String, String?)> template;
+  final List<(String, String?)> _template;
 
   Template.empty()
-      : objects = [],
-        template = [];
+      : _name = 'default',
+        objects = [],
+        _template = [];
+
+  List<(String, String?)> get template => _template;
+
+  set name(String value) => _name = value;
 
   Map<String, dynamic> toMap() => {
-        'objects': Map.fromEntries(objects.map((entry) =>
-            MapEntry(entry.$1, Map.fromEntries(entry.$2.map((e) =>
-                MapEntry(e.$1, e.$2)))))),
-        'template': Map.fromEntries(template.map((entry) => MapEntry(entry.$1, entry.$2))),
+        'objects': Map.fromEntries(
+            objects.map((entry) => MapEntry(entry.$1, Map.fromEntries(entry.$2.map((e) => MapEntry(e.$1, e.$2)))))),
+        'template': Map.fromEntries(_template.map((entry) => MapEntry(entry.$1, entry.$2))),
+        'name': _name,
       };
 
   @override
